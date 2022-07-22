04f80ee0a447bdd4c135eb987983f0219a217b7b
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-21 23:47:26 -0400

    Started navbar

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <>
+        <h1 className="text-center">PINELLAS PREMIUM LLC</h1>
+        <h2 className="text-center">MOWING LANDSCAPING CLEAN-UPS</h2>
+        <Navbar />
+    </>
+  )
+}
+
+export default Header
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,18 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+import { Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+
+const CustomNavbar = () => {
+  return <Navbar>
+    <Navbar.Brand>Pinellas Premium, LLC</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to='/'><Nav.Link>Home</Nav.Link></LinkContainer>
+            <LinkContainer to='/about'><Nav.Link>About</Nav.Link></LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+  </Navbar>
 }
 
-export default Navbar
+export default CustomNavbar
